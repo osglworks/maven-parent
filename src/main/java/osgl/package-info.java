cd157834a06d326f47ad0377d9d907630857d120
Gelin Luo <greenlaw110@gmail.com>
2017-10-03 19:24:51 +1100

    first check in

---
+++ src/main/java/osgl/package-info.java
@@ -0,0 +1,28 @@
+/**
+ * Parent project.
+ *
+ * @author Gelin Luo (greenlaw110@gmail.com)
+ * @version $Id$
+ * @since 2.0
+ */
+package osgl;
+
+/*-
+ * #%L
+ * parent
+ * %%
+ * Copyright (C) 2017 OSGL (Open Source General Library)
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
